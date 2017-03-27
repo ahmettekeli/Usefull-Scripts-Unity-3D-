5ad8bccf31c0d06a66fd020bce472a248c95027e
Ahmet Tekeli <ahmettekeli@users.noreply.github.com>
2017-03-27 18:21:02 +0300

    Game Music Playlist Manage Script
    Usefull to make a game music playlist and play them one after the other in random order.

---
+++ GameMusicManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMusicManager : MonoBehaviour {
+
+	//object need an AudioSource component (Obviously)
+    public AudioClip[] musicList;
+    public AudioSource audio;
+
+    void Start()
+    {
+        PlayNextSong();
+    }
+
+	//waits until the current music ends and randomly starts playing an audioclip from the array 
+    void PlayNextSong()
+    {
+        audio.clip = musicList[Random.Range(0, musicList.Length)];
+        audio.Play();
+        Invoke("PlayNextSong", audio.clip.length+1);
+    }
+}
